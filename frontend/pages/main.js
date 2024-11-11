8a960727f47be7bc5cbe28a8f44eb82014fad1e0
Jayashre <jaya2004kra@gmail.com>
2024-11-11 23:09:06 +0530

    added head content

--- frontend/pages/main.js
+++ frontend/pages/main.js
@@ -3,15 +3,22 @@ import Footer from './components/footer';
 import Header from './components/header';
 import CalltoAction from './components/main/calltoaction';
 import HowItWorks from './components/main/howitworks';
+import Head from "next/head";
 
 const Main = () => {
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-900">
+    <>
+    <Head>
+        <title>SandalwoodAI</title>
+        <meta name="description" content="An AI-powered platform for processing and understanding Kannada audio resources on sandalwood cultivation, enabling users to query and explore indigenous knowledge through speech recognition." />
+      </Head>
+    <div className="min-h-screen">
       <Header />
       <CalltoAction />
       <HowItWorks />
       < Footer />
     </div>
+    </>
   );
 };
 
