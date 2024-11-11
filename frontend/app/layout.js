8a7f24fbb8ce80bc39828d3aa376e29e07eae4e7
Jayashre <jaya2004kra@gmail.com>
2024-11-11 12:41:45 +0530

    added fonts

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,4 +1,23 @@
 import "./globals.css";
+import {Lexend_Deca, League_Spartan, Almarai} from "next/font/google"
+
+const lecend_dexa_init = Lexend_Deca({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-lexend-deca',
+});
+
+const league_spartan_init = League_Spartan({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-league-spartan',
+});
+
+const almarai_init = Almarai({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-almarai',
+});
 
 export const metadata = {
   title: "SandalwoodAI",
@@ -8,9 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className="antialiased"
-      >
+      <body className={`${almarai_init.variable} ${league_spartan_init.variable} ${lecend_dexa_init.variable}`}>
         {children}
       </body>
     </html>
