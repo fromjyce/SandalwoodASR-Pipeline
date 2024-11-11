2c42c05e9599900874eaebb2bdb1e27d990f4d5d
Jayashre <jaya2004kra@gmail.com>
2024-11-11 12:19:03 +0530

    added footer

---
+++ frontend/pages/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer py-2">
+        <div className="max-w-7xl mx-auto text-center text-white">
+          <p>Developed by <strong>Team Matrix-Odyssey</strong> for <strong>ML-Fiesta: The AI/ML Hackathon</strong> organized by the <strong>International Institute of Information Technology, Bangalore (IIITB)</strong>.</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
