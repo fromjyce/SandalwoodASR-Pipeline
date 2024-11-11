8c7ad8ccdb10fa760f4f629e703563aeb021fdb2
Jayashre <jaya2004kra@gmail.com>
2024-11-11 12:54:08 +0530

    added colour

--- frontend/pages/components/footer.js
+++ frontend/pages/components/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <footer className="footer py-2">
-        <div className="max-w-7xl mx-auto text-center text-white">
+      <footer className="main-footer py-2">
+        <div className="max-w-7xl mx-auto text-center text-white lexend_deca">
           <p>Developed by <strong>Team Matrix-Odyssey</strong> for <strong>ML-Fiesta: The AI/ML Hackathon</strong> organized by the <strong>International Institute of Information Technology, Bangalore (IIITB)</strong>.</p>
         </div>
       </footer>
