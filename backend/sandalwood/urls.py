0cd4eb344cca3062e71c4c059ecf92e7255e65e3
Jayashre <jaya2004kra@gmail.com>
2024-11-16 18:40:47 +0530

    added new link

--- backend/sandalwood/urls.py
+++ backend/sandalwood/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import submit_text
+from .views import submit_text, submit_query
 
 urlpatterns = [
     path('submit_text/', submit_text, name='submit_text'),
+    path('submit_query/', submit_query, name='submit_query'),
 ]
