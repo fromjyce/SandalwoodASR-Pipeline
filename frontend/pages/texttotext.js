66f62a1f150341cc3daf4176868363170c0c12ac
Jayashre <jaya2004kra@gmail.com>
2024-11-16 23:05:56 +0530

    added final code

--- frontend/pages/texttotext.js
+++ frontend/pages/texttotext.js
@@ -115,8 +115,8 @@ const TextToText = () => {
           <div className="mt-8 w-full max-w-lg bg-[#f9decd] items-center justify-center text-center p-6 rounded-lg shadow-lg">
             <h2 className="text-xl font-semibold poppins stt-option-choose mb-2">Results</h2>
             <div className="font-medium">
-              <p><strong>Answer:</strong> {results.answer}</p>
-              <p><strong>Filename:</strong> {results.filename}</p>
+              <p>{results.answer}</p>
+              <h5 className='text-md font-semibold poppins stt-option-choose mt-3'>Play this Audio!</h5>
               {results.filename && (
                 <audio controls className="mt-4 w-full">
                   <source src={`/Sandalwood_Audios/${results.filename}`} type="audio/wav" />
